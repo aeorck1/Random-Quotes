7b7f4f1c1e67c0b2b0f5bd4efda853e694d65394
Godwin Daniel <lemuelgodwin2@gmail.com>
2023-02-16 23:54:47 +0100

    Added Javascript

---
+++ rando.js
@@ -0,0 +1,12 @@
+const quote=document.getElementById("quoter");
+const author=document.getElementById("author");
+
+const quotes_authors={
+    quote: "Die if you give up, live when you don\'t",
+    author: "Joe Brown"
+}
+
+function quote_gen() {
+quote.innerHTML=quotes_authors.quote
+author.innerHTML=quotes_authors.author    
+}
